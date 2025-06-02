e6567ff0e342c68d281fd91a53c540ee0c8d4c8c
NSenseQuantum <nsensequantum@gmail.com>
2025-06-02 15:43:16 +0100

    Fix TypeScript errors and optimize server build

--- server/storage.ts
+++ server/storage.ts
@@ -1,5 +1,21 @@
 import { users, type User, type InsertUser } from "@shared/schema";
 
+interface Contact {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  message: string;
+  createdAt: Date;
+}
+
+interface InsertContact {
+  firstName: string;
+  lastName: string;
+  email: string;
+  message: string;
+}
+
 // modify the interface with any CRUD methods
 // you might need
 
@@ -7,15 +23,21 @@ export interface IStorage {
   getUser(id: number): Promise<User | undefined>;
   getUserByUsername(username: string): Promise<User | undefined>;
   createUser(user: InsertUser): Promise<User>;
+  createContact(contact: InsertContact): Promise<Contact>;
+  getAllContacts(): Promise<Contact[]>;
 }
 
 export class MemStorage implements IStorage {
   private users: Map<number, User>;
+  private contacts: Map<number, Contact>;
   currentId: number;
+  currentContactId: number;
 
   constructor() {
     this.users = new Map();
+    this.contacts = new Map();
     this.currentId = 1;
+    this.currentContactId = 1;
   }
 
   async getUser(id: number): Promise<User | undefined> {
@@ -33,6 +55,21 @@ export class MemStorage implements IStorage {
     const user: User = { ...insertUser, id };
     this.users.set(id, user);
     return user;
+  }
+
+  async createContact(contact: InsertContact): Promise<Contact> {
+    const id = this.currentContactId++;
+    const newContact: Contact = {
+      ...contact,
+      id,
+      createdAt: new Date()
+    };
+    this.contacts.set(id, newContact);
+    return newContact;
+  }
+
+  async getAllContacts(): Promise<Contact[]> {
+    return Array.from(this.contacts.values());
   }
 }
 
